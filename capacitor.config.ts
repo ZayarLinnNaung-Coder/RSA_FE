47d677455dc348ba3b9aa4e5e849c064e57e8535
Zayar Linn Naung <84829442+ZayarLinnNaung-Coder@users.noreply.github.com>
2024-07-28 22:46:50 +0630

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.VoltHunger',
   appName: 'VoltHunger',
   webDir: 'www'
 };
--- src/app/constants/UrlConstants.ts
+++ src/app/constants/UrlConstants.ts
@@ -1,5 +1,6 @@
 export class UrlConstants{
 
-    public static BASE_URL: string = 'http://localhost:8080'
+    // public static BASE_URL: string = 'http://localhost:8080';
+    public static BASE_URL: string = 'https://rsa-production-app.up.railway.app';
     
 }
