0b597ec1ff1ef8c031affd766f09fbefa8a07fc0
Zayar Linn Naung <84829442+ZayarLinnNaung-Coder@users.noreply.github.com>
2024-08-10 23:20:28 +0630

    x

--- src/app/home/lock-page/lock.page.ts
+++ src/app/home/lock-page/lock.page.ts
@@ -9,27 +9,36 @@ import { UrlConstants } from 'src/app/constants/UrlConstants';
 })
 export class LockPage{
 
+  showLoading = false;
+
   responseText: string = ''
+  disabledTextArea = false
+  disabledTxtFileInput = false
+  disabledFileInput = false
   
   @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+  @ViewChild('txtFileInput', { static: false }) txtFileInput!: ElementRef;
 
   constructor(private http: HttpClient){}
 
   encryptData(publicKeyPair: string, message: string) {
 
+    this.showLoading = true
     let selectedFile = this.fileInput.nativeElement.files[0];
+    if(!selectedFile){
+      selectedFile = this.txtFileInput.nativeElement.files[0];
+    }
+
     if(selectedFile){
 
       const url = UrlConstants.BASE_URL + '/rsa/encrypt/file';
-
 
       let formData = new FormData();
       formData.append('publicKeyPair', publicKeyPair)
       formData.append('file', selectedFile)
 
-      console.log(selectedFile)
-
       this.http.post(url, formData, { responseType: 'blob' }).subscribe(blob => {
+        this.showLoading = false
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
@@ -47,9 +56,28 @@ export class LockPage{
 
       this.http.post(UrlConstants.BASE_URL + "/rsa/encrypt/message", formData).subscribe((response: any) => {
         this.responseText = response.message
+        this.showLoading = false
       })
     }
 
+  }
+
+  changeTextArea(message: string) {
+    const result = (message != '')
+    this.disabledFileInput = result
+    this.disabledTxtFileInput = result
+  }
+
+  changeTxtFileInput() {
+    const result = this.txtFileInput.nativeElement.files.length != 0
+    this.disabledTextArea = result
+    this.disabledFileInput = result
+  }
+
+  changeFileInput() {
+    const result = this.fileInput.nativeElement.files.length != 0
+    this.disabledTextArea = result
+    this.disabledTxtFileInput = result
   }
 
 }
--- src/app/home/unlock-page/unlock.page.ts
+++ src/app/home/unlock-page/unlock.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import {Component, ElementRef, ViewChild } from '@angular/core';
 import { UrlConstants } from 'src/app/constants/UrlConstants';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-unlock',
@@ -10,14 +11,26 @@ import { UrlConstants } from 'src/app/constants/UrlConstants';
 export class UnlockPage{
 
   responseText: string = ''
+  disabledTextArea = false
+  disabledFileInput = false
+  disabledTxtFileInput = false
+
+  showLoading = false
   
   @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+  @ViewChild('txtFileInput', { static: false }) txtFileInput!: ElementRef;
 
   constructor(private http: HttpClient){}
 
   decryptData(privateKeyPair: string, message: string) {
 
+    this.showLoading = true
+
     let selectedFile = this.fileInput.nativeElement.files[0];
+    if(!selectedFile){
+      selectedFile = this.txtFileInput.nativeElement.files[0];
+    }
+
     if(selectedFile){
 
       const url = UrlConstants.BASE_URL + '/rsa/decrypt/file';
@@ -26,18 +39,22 @@ export class UnlockPage{
       formData.append('privateKeyPair', privateKeyPair)
       formData.append('file', selectedFile)
 
-      console.log(selectedFile)
+      // this.http.post(url, formData, { responseType: 'blob' }).subscribe(blob => {
+      //   this.showLoading = false
+      //   const url = window.URL.createObjectURL(blob);
+      //   const a = document.createElement('a');
+      //   a.href = url;
+      //   a.download = selectedFile.name;
+      //   document.body.appendChild(a);
+      //   a.click();
+      //   document.body.removeChild(a);
+      // })
+
 
       this.http.post(url, formData, { responseType: 'blob' }).subscribe(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = selectedFile.name;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
+        this.showLoading = false
+        saveAs(blob, selectedFile.name);
       })
-
     } else {
 
       let formData = new FormData();
@@ -46,9 +63,29 @@ export class UnlockPage{
 
       this.http.post(UrlConstants.BASE_URL + "/rsa/decrypt/message", formData).subscribe((response: any) => {
         this.responseText = response.message
+        this.showLoading = false
       })
     }
 
+  }
+
+
+  changeTextArea(message: string) {
+    const result = (message != '')
+    this.disabledFileInput = result
+    this.disabledTxtFileInput = result
+  }
+
+  changeTxtFileInput() {
+    const result = this.txtFileInput.nativeElement.files.length != 0
+    this.disabledTextArea = result
+    this.disabledFileInput = result
+  }
+
+  changeFileInput() {
+    const result = this.fileInput.nativeElement.files.length != 0
+    this.disabledTextArea = result
+    this.disabledTxtFileInput = result
   }
 
 }
